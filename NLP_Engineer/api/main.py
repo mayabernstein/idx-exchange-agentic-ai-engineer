bf3d994a2f245f38464fb3ba5fc3f9cc0dc42515
Maya <maya.bernstein016@gmail.com>
2026-09-07 23:17:04 -0500

    main - nlp_engineer

--- NLP_Engineer/api/main.py
+++ NLP_Engineer/api/main.py
@@ -41,7 +41,8 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173",
+                   "https://real-estate-recommendation-system-production.up.railway.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
